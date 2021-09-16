8bded0074accc0a699aae42e6ce7e615a33f93c5
Phil Vigus <phil.vigus@mumsnet.com>
2021-09-16 13:00:50 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Alpine Js</title>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <livewire:styles />
+</head>
+
+<body>
+
+    <div x-data="{ show: false }">
+        <button @click="show = !show">Show</button>
+        <h1 x-show="show">Alpine Js is working !</h1>
+    </div>
+    <hr>
+
+    <div x-data>
+        <button @click="alert('Alpine Js is working !')">Click</button>
+    </div>
+    <livewire:scripts />
+
+</body>
+
+</html>
